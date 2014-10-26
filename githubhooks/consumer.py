13bf818cd3406d42739b3e20428e50b635e2930a
Dan Foster <dan@zem.org.uk>
2014-10-26 15:43:07 +0000

    Add useragent check

--- githubhooks/consumer.py
+++
@@ -1,5 +0,0 @@
-from flask.ext import restful
-
-class Consumer(restful.Resource):
-    def post(self):
-        return {'hello': 'world'}
--- githubhooks/server.py
+++ githubhooks/server.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
-from githubhooks.consumer import Consumer
-from flask import Flask
-from flask.ext import restful
+import githubhooks
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def consume_post():
+    useragent = request.headers.get('User-Agent')
+    if useragent == None or not useragent.startswith('GitHub-Hookshot'):
+        print "ERROR: Incorrect client user-agent, rejecting request"
+        return ""
+    return ""
 
 def server():
-    app = Flask(__name__)
-    api = restful.Api(app)
-    api.add_resource(Consumer, '/')
     app.run(host='0.0.0.0',debug=True)
 
 if __name__ == '__main__':
